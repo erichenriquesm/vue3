2fc8f33ee01e3b7774c474632b7a3ebadf553412
Eric Motta <eric@greenn.com.br>
2024-08-07 19:35:59 -0300

    ok

--- src/components/Usuario.vue
+++ src/components/Usuario.vue
@@ -14,21 +14,46 @@ import {
 
 interface IPerson {
     name: string,
-    birth: number
+    birth: number,
+    avatar: string,
+    email: string
 }
 
-const name = ref('Eric Motta');
-const birth = ref(2000);
+interface IPersonRequestData {
+    id: number,
+    first_name: string,
+    last_name: string,
+    avatar: string,
+    email: string
+}
+
+// const name = ref('Eric Motta');
+// const birth = ref(2000);
 
 // const calcAge = function () : void {
 //     age.value = 2024 - birth.value;
 // }
 
-const age = computed(() => {
-    return 2024 - birth.value;
+
+const person:IPerson = reactive({
+    name: '',
+    birth: 2000,
+    avatar: '',
+    email: ''
 });
 
+const age = computed(() => {
+    return 2024 - person.birth;
+});
 
+const fetchPerson = async function () {
+    const request = await fetch(`https://reqres.in/api/users/${1}`); 
+    const personData:IPersonRequestData = (await request.json()).data;
+
+    person.name = personData.first_name + personData.last_name;
+    person.avatar = personData.avatar;
+    person.email = personData.email;
+}
 
 onBeforeMount(() => {
     console.log('componente criado');
@@ -36,6 +61,11 @@ onBeforeMount(() => {
 
 onMounted(() => {
     console.log('componente montado');
+    fetchPerson();
+    person.name = 'Eric Motta';
+    person.birth = 2005;
+    person.avatar = 'https://img.freepik.com/fotos-gratis/reflexo-das-luzes-e-da-montanha-em-um-lago-capturado-em-parco-ciani-lugano-suica_181624-24209.jpg?t=st=1722993613~exp=1722997213~hmac=22e37a40f9c0f18aff76d9b96bacaf241817cd9103639c5d6b843f2594b78bda&w=1380';
+    person.email = 'eric@greenn.com.br';
 });
 
 onBeforeUpdate(() => {
@@ -54,31 +84,32 @@ onUnmounted(() => {
     console.log('componente após desmontar');
 });
 
-// const person:IPerson = reactive({
-//     name: '',
-//     birth: 2000
-// });
-
 // birth.value = 2005;
 
 </script>
 
 <template>
     <div>
-        <h1>{{ name }} tem {{ age }} anos</h1>
+
+        <div class="profile">
+            <img class="avatar" :src="person.avatar" alt="avatar">
+            <h1>{{ person.name }}</h1>
+            <h2>{{ person.email }}</h2>
+        </div>
+        <!-- <h1>{{ person.name }} tem {{ age }} anos</h1>
 
 
         <div>
             <div>
                 <label for="name">Digite seu nome</label>
-                <input type="text" id="name" v-model="name">
+                <input type="text" id="name" v-model="person.name">
             </div>
 
             <div>
                 <label for="name">Digite o ano do seu nascimento</label>
-                <input type="number" id="name" v-model="birth">
+                <input type="number" id="name" v-model="person.birth">
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
 
@@ -90,5 +121,17 @@ input {
     display: block;
     padding: 3px;
     margin-bottom: 10px;
+}
+.profile {
+    display: grid;
+    gap: 10px;
+    width: fit-content;
+    padding: 20px 60px;
+    justify-items: center;
+}
+.avatar {
+    width: 200px;
+    height: 200px;
+    border-radius: 15px;
 }
 </style>
